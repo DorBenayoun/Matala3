c9f943ffc0b96d82f9b8300f5eaf075e17a8505a
DorBenayoun <74483949+DorBenayoun@users.noreply.github.com>
2021-01-05 03:35:26 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 50
+#include "isort.h"
+
+int main(){
+  int a[size];			
+  int n;
+ for(int i = 0;i < size; i++){
+    printf("enter number: ");
+    scanf("%d", (a+i));
+}
+  insertion_sort(a,size);
+  for(int i = 0; i < size; i++){
+    printf("%d", *(a+i));
+     if(i == 49){
+       break;
+     }
+   printf(",");
+  }
+  printf("\n");
+  return 0;
+}
